b90d6b1af47d4dd493b5d91374db839be62f07cb
nhuhaidev <dinhnhuhai05@gmail.com>
2023-09-30 17:10:52 +0700

    update a little bit

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,7 +1,6 @@
 #include <iostream>;
 #include <vector>;
 using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -10,13 +9,13 @@ public:
         {
             for (int j = i + 1; j < n; j++)
             {
-                if (nums[i] + nums[j] == targe)
+                if (nums[i] + nums[j] == target)
                 {
                     return {i, j};
                 }
             }
         }
 
-        return {};
+      return {};
     }
 };
